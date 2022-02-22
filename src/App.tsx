86d06e7b2a991904f67cd09f62f3b98cef419e1b
Jayaram Nair <jayaramsivaramannair@gmail.com>
2022-02-22 05:32:03 -0500

    Created a component called Shell to hold the timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Shell from './components/Shell'
 
 function App() {
   return (
     <div className="App">
-      Hello Twenty Five Calculator
+      <Shell />
     </div>
   );
 }
---
+++ src/components/Shell.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Shell() {
+  return (
+    <div className="timer-shell">
+      <p>25 + 5 Clock</p>
+    </div>
+  )
+}
+
+export default Shell
