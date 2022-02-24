0eb1509f8f1d1749b6be50ccf9e8f89916bcba08
Jayaram Nair <jayaramsivaramannair@gmail.com>
2022-02-23 20:35:49 -0500

    Added clickhandler functions to increment and decrement session length

--- src/components/SessionLength.tsx
+++ src/components/SessionLength.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import {BiUpArrow} from 'react-icons/bi'
 import {BiDownArrow} from 'react-icons/bi'
+import {RootState} from '../store/store'
+import {useSelector, useDispatch} from 'react-redux'
+import {incrementSession, decrementSession} from '../store/timerSlice'
 
 function SessionLength() {
+
+  const sessionLength = useSelector((state: RootState) => state.timer.sessionLength)
+  const dispatch = useDispatch()
+
   return (
     <div id="session-label">
       <p>Session Length</p>
         <div className="session-icons">
-          <BiDownArrow id="session-decrement"/>
-          <p id="session-length">25</p>
-          <BiUpArrow id="session-increment"/>
+          <div>
+            <BiDownArrow id="session-decrement" onClick={() => dispatch(decrementSession())}/>
+          </div>
+          <p id="session-length">{sessionLength}</p>
+          <div>
+            <BiUpArrow id="session-increment" onClick={() => dispatch(incrementSession())}/>
+          </div>
         </div>
     </div>
   )
